c61fca7b2989eb23ce199b0b2781db5984238259
Ian Scott Martin <ianscottmartin@gmail.com>
2023-07-09 15:04:16 -0600

    fibon

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,14 @@
 
 def print_fibonacci(length):
     pass
+    fibonacci_seq = []
+    if length > 0:
+        fibonacci_seq.append(0)
+        if length > 1:
+            fibonacci_seq.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    fibonacci_seq.append(
+                        fibonacci_seq[i-1] + fibonacci_seq[i - 2])
+                    
+    print(fibonacci_seq)
